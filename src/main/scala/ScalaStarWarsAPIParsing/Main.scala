8d9eb5877bf5f9f0c083277dd615930ebc3e6d3f
manny0013 <manny0013@gmail.com>
2018-10-04 19:22:41 -0400

    Add HTTP library

--- src/main/scala/ScalaStarWarsAPIParsing/Main.scala
+++ src/main/scala/ScalaStarWarsAPIParsing/Main.scala
@@ -1,6 +1,19 @@
+
 object Main extends App {
+  def abs(x: Double) = if (x >= 0) x else -x
 
-  def area(radius: Double): Double = 3.14159 * square(radius)
+  def sqrtIter(guess: Double, x: Double): Double =
+    if (isGoodEnough(guess, x)) guess
+    else sqrtIter(improve(guess, x), x)
 
-  println(area(10))
+  def improve(guess: Double, x: Double) =
+    (guess + x / guess) / 2
+
+  def isGoodEnough(guess: Double, x: Double) =
+    abs(guess * guess - x) < 0.001
+
+  def sqrt(x: Double) = sqrtIter(1.0, x)
+
+
+  println("\n\n\n"++sqrt(256).toInt.toString())
 }
