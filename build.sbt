fc92f79270c0595705206050a74d1575ae4a20e5
manny0013 <manny0013@gmail.com>
2018-10-04 19:41:14 -0400

    HTTP request fetch add

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "ScalaStarWarsAPIParsing"
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.1"
--- src/main/scala/ScalaStarWarsAPIParsing/Main.scala
+++ src/main/scala/ScalaStarWarsAPIParsing/Main.scala
@@ -1,19 +1,8 @@
 
+import scalaj.http.Http
+
 object Main extends App {
-  def abs(x: Double) = if (x >= 0) x else -x
+  val BaseURL = "https://swapi.co/api/people/"
 
-  def sqrtIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
-
-  def improve(guess: Double, x: Double) =
-    (guess + x / guess) / 2
-
-  def isGoodEnough(guess: Double, x: Double) =
-    abs(guess * guess - x) < 0.001
-
-  def sqrt(x: Double) = sqrtIter(1.0, x)
-
-
-  println("\n\n\n"++sqrt(256).toInt.toString())
+  println(Http(BaseURL).asString)
 }
