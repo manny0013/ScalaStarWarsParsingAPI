e570be71c75cc4efaf9165aa5d512a275a9cb095
manny0013 <manny0013@gmail.com>
2018-10-05 14:56:36 -0400

    Loop through all People and fetch JSON, add to list

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ version := "0.1"
 
 scalaVersion := "2.12.7"
 
-libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.4.1"
+libraryDependencies += "com.lihaoyi" %% "requests" % "0.1.4"
--- src/main/scala/ScalaStarWarsAPIParsing/Main.scala
+++ src/main/scala/ScalaStarWarsAPIParsing/Main.scala
@@ -1,10 +1,27 @@
 
-import scalaj.http.{Http, HttpResponse}
+import requests._
+
+import scala.collection.mutable.ListBuffer
 
 object Main extends App {
 
-  def fetch(URL: String): HttpResponse[String] = Http(URL).asString
-  val BaseURL = "https://swapi.co/api/people/1/"
+  def fetch(URL: String): Response = get(URL)
 
-  println(fetch(BaseURL))
+  val BaseURL: String = "https://swapi.co/api/people/"
+
+  val MainPage: List[String] = fetch(BaseURL).data.text.split(",").toList
+  val TotalPages = MainPage(0).split(":")(1)
+  println(TotalPages)
+  var ParsedData = new ListBuffer[String]()
+  var TotalCount = ParsedData.length
+  var x = 1
+  while(TotalCount < TotalPages.toInt){
+    val fetchedData = fetch(BaseURL++x.toString++"/")
+    x+=1
+    println(fetchedData.data.text)
+    if (fetchedData.statusCode != 404) {
+      ParsedData += fetchedData.data.text
+      TotalCount = ParsedData.length
+    }
+  }
 }
